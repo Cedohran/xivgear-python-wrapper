f69f85a0673bf4a47d9d007912579b8b1bba6fc3
Cedohran <cedohran@gmx.de>
2026-01-25 15:46:37 +0100

    (feat): add xivgear set retrieval

--- src/data_structures/xiv_gear_item.py
+++ src/data_structures/xiv_gear_item.py
@@ -8,7 +8,7 @@ class XIVGearSlot(str, Enum):
     BODY = "Body"
     HAND = "Hand"
     LEGS = "Legs"
-    Feet = "Feet"
+    FEET = "Feet"
     EARS = "Ears"
     NECK = "Neck"
     WRIST = "Wrist"
@@ -29,10 +29,23 @@ class GearTier(str, Enum):
 
 
 class XIVGearItem():
-    def __init__(self, id: int,
+    def __init__(self, itemid: str,
                  gear_slot: XIVGearSlot,
-                 materia: List[int]):
-        self.id = id
+                 materia: List[str]):
+        self.itemid = itemid
         self.gear_slot = gear_slot
         self.materia = materia
 
+    def __eq__(self, other):
+        # enable == None
+        if self.itemid == "None":
+            return None
+
+        # don't attempt to compare against unrelated types
+        if not isinstance(other, XIVGearItem):
+            return NotImplemented
+        
+        return self.itemid == other.itemid
+
+    def __str__(self):
+        return ""
